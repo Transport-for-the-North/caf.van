b5fc1b2e82947dc4bb0166b54d9b7d79b207bc94
Kieran-Fishwick-TfN <kieran.fishwick@transportforthenorth.com>
2024-12-06 11:21:03 +0000

    added some scripts

--- src/scripts/combine_dvec.py
+++ src/scripts/combine_dvec.py
@@ -51,22 +51,23 @@ def zone_dvec(in_path: str, out_path: str) -> None:
     )  # .save(out_path)
 
 
-# zone_dvec(r"F:\Working\Land-Use\OUTPUTS_base_employment_inc_scotland_v2_voa_and_farmers_adjust\Output E5.hdf", r"U:\Lot3_LFT\2.LGV Model\2024 - LGVN Rebase to 2023\inputs\TfN-Land-Use-Pop\jobs.dvec")
+
+zone_dvec(r"F:\Working\Land-Use\OUTPUTS_base_employment_bres_approach_a_weighting_2_level_check\02_Final Outputs\Output E6.hdf", r"U:\Lot3_LFT\2.LGV Model\2024 - LGVN Rebase to 2023\inputs\TfN-Land-Use-Pop\jobs-26-11-24.dvec")
 
 
 concat_dvecs(
     pathlib.Path(
-        r"F:\Working\Land-Use\DELIVERABLES\20241111_POPULATION\02_Final Outputs\*P11.1*.hdf"
+        r"F:\Deliverables\Land-Use\241123_Population rebase\02_Final Outputs\*P11.1*.hdf"
     ),
     pathlib.Path(
-        r"U:\Lot3_LFT\2.LGV Model\2024 - LGVN Rebase to 2023\inputs\TfN-Land-Use-Pop\occupied_dwellings_v2.dvec"
+        r"U:\Lot3_LFT\2.LGV Model\2024 - LGVN Rebase to 2023\inputs\TfN-Land-Use-Pop\occupied_dwellings_26-11-24.dvec"
     ),
 )
 concat_dvecs(
     pathlib.Path(
-        r"F:\Working\Land-Use\DELIVERABLES\20241111_POPULATION\01_Intermediate Files\*P11.2*.hdf"
+        r"F:\Deliverables\Land-Use\241123_Population rebase\01_Intermediate Files\*P11.2*.hdf"
     ),
     pathlib.Path(
-        r"U:\Lot3_LFT\2.LGV Model\2024 - LGVN Rebase to 2023\inputs\TfN-Land-Use-Pop\unoccupied_dwellings_v2.dvec"
+        r"U:\Lot3_LFT\2.LGV Model\2024 - LGVN Rebase to 2023\inputs\TfN-Land-Use-Pop\unoccupied_dwellings_26-11-24.dvec"
     ),
 )
