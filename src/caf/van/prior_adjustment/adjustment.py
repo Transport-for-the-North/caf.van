dabbeea7b3e7105d66f3e1c3d5b81a6a6c0382c3
KieranFishwick <kieran.fishwick@transportforthenorth.com>
2025-06-12 08:54:36 +0100

    General bug fixes

--- src/caf/van/prior_adjustment/adjustment.py
+++ src/caf/van/prior_adjustment/adjustment.py
@@ -1,8 +1,11 @@
 import dataclasses
+from functools import reduce
 import logging
 import pathlib
 import warnings
 
+
+import tqdm.contrib.logging as tqdm_log
 import pandas as pd
 import caf.toolkit as ctk
 from caf.distribute import gravity_model, cost_functions, furness
@@ -19,8 +22,8 @@ from caf.van import lgv_inputs, lgv_model, utilities, __version__
 
 LOG = logging.getLogger(__name__)
 
-ANNUAL_MATRIX_DIR = "annual_matrices"
-
+INTERMEDIARY_MATRIX_DIR = "intermediary_matrices"
+ANNUAL_MATRICES_DIR = "annual_matrices"
 
 @dataclasses.dataclass
 class PriorAdjustmentMatrix:
@@ -36,23 +39,23 @@ class PriorAdjustmentMatrix:
 
     @property
     def annual_od_matrix_path(self) -> str:
-        return f"{ANNUAL_MATRIX_DIR}/{self.name}_prior_annual_od_matrix.csv"
+        return f"{INTERMEDIARY_MATRIX_DIR}/{self.name}_prior_annual_od_matrix.csv"
 
     @property
     def annual_network_od_matrix_path(self) -> str:
-        return f"{ANNUAL_MATRIX_DIR}/{self.name}_network_prior_annual_od_matrix.csv"
+        return f"{INTERMEDIARY_MATRIX_DIR}/{self.name}_network_prior_annual_od_matrix.csv"
 
     @property
     def annual_sector_od_matrix_path(self) -> str:
-        return f"{ANNUAL_MATRIX_DIR}/{self.name}_sector_prior_annual_od_matrix.csv"
+        return f"{INTERMEDIARY_MATRIX_DIR}/{self.name}_sector_prior_annual_od_matrix.csv"
 
     @property
     def annual_network_pa_matrix_path(self) -> str:
-        return f"{ANNUAL_MATRIX_DIR}/{self.name}_network_prior_annual_pa_matrix.csv"
+        return f"{INTERMEDIARY_MATRIX_DIR}/{self.name}_network_prior_annual_pa_matrix.csv"
 
     @property
     def annual_sector_pa_matrix_path(self) -> str:
-        return f"{ANNUAL_MATRIX_DIR}/{self.name}_sector_prior_annual_pa_matrix.csv"
+        return f"{INTERMEDIARY_MATRIX_DIR}/{self.name}_sector_prior_annual_pa_matrix.csv"
 
     def write_annual_od_matrix(
         self, tp_to_annual_factors: dict[str, float], output_path: pathlib.Path
@@ -69,8 +72,10 @@ class PriorAdjustmentMatrix:
             read_annual_matrix(self.od_prior_matrices, tp_to_annual_factors).to_csv(path)
 
     def get_annual_od_matrix(self, output_path: pathlib.Path) -> pd.DataFrame:
-        return pd.read_csv(output_path / self.annual_od_matrix_path, index_col=0)
-
+        matrix = pd.read_csv(output_path / self.annual_od_matrix_path, index_col=0)
+        matrix.columns = [int(col) for col in matrix.columns]
+        return matrix
+ 
     def get_network_annual_od_matrix(
         self, output_path: pathlib.Path, translation: pd.DataFrame
     ) -> pd.DataFrame:
@@ -80,17 +85,21 @@ class PriorAdjustmentMatrix:
                 f"network OD matrix already exists at {path}."
                 " Continuing with written file. If that is not desired, please delete the file."
             )
-            return pd.read_csv(path, index_col=0)
-        else:
-            matrix = ctk.translation.pandas_matrix_zone_translation(
-                self.get_annual_od_matrix(output_path),
-                translation,
-                "from",
-                "to_network",
-                "factors",
-            )
-            matrix.to_csv(path)
+            matrix = pd.read_csv(path, index_col=0)
+            matrix.columns = [int(col) for col in matrix.columns]
             return matrix
+    
+    
+        matrix = ctk.translation.pandas_matrix_zone_translation(
+            self.get_annual_od_matrix(output_path),
+            translation,
+            "from",
+            "to_network",
+            "factors",
+        )
+        matrix.to_csv(path)
+        matrix.columns = [int(col) for col in matrix.columns]
+        return matrix
 
     def get_sector_annual_od_matrix(
         self, output_path: pathlib.Path, translation: pd.DataFrame
@@ -130,11 +139,15 @@ class PriorAdjustmentMatrix:
                 f"network PA matrix already exists at {path}."
                 " Continuing with written file. If that is not desired, please delete the file."
             )
-            return pd.read_csv(path, index_col=0)
+            matrix = pd.read_csv(path, index_col=0)
+            matrix.columns = [int(col) for col in matrix.columns]
+            return matrix
         matrix = ctk.translation.pandas_matrix_zone_translation(
             self.get_annual_pa_matrix(), translation, "from", "to_network", "factors"
         )
+        matrix.columns = [int(col) for col in matrix.columns]
         matrix.to_csv(path)
+        
         return matrix
 
     def get_sector_annual_pa_matrix(
@@ -161,18 +174,21 @@ class PriorAdjustmentMatrix:
         translations: pd.DataFrame,
         cost_matrix_path: pd.DataFrame,
         output_path: pathlib.Path,
-    ):
+    ) -> pd.DataFrame:
+        LOG.debug("adjusting tripends")
         if self.pa_prior_matrices is not None:
             prior_matrix = self.get_annual_pa_matrix()
-            sector_prior_matrix = self.get_sector_annual_pa_matrix(output_path, translations)
         else:
             prior_matrix = self.get_annual_od_matrix(output_path)
+
+        matrix_output_dir = output_path / ANNUAL_MATRICES_DIR
+        matrix_output_dir.mkdir(parents=True, exist_ok=True)
 
         adj_factors, sector_adj_factors = calculate_trip_end_adjustment_factors(
             prior_matrix, post_me_matrix, translations
         )
         sector_adj_factors.to_csv(
-            output_path / f"{self.name}_sector_trip_end_adjustment_factors.csv"
+            matrix_output_dir / f"{self.name}_sector_trip_end_adjustment_factors.csv"
         )
         del sector_adj_factors
 
@@ -200,11 +216,9 @@ class PriorAdjustmentMatrix:
 
         adj_trip_ends = adj_trip_ends.drop(columns=["demand", "row_factors", "column_factors"])
 
-        adj_trip_ends.to_csv(output_path / f"{self.name}_adjusted_trip_ends.csv", index=False)
+        adj_trip_ends.to_csv(matrix_output_dir / f"{self.name}_adjusted_trip_ends.csv", index=False)
 
-        sector_target_matrix = ctk.translation.pandas_matrix_zone_translation(
-            post_me_matrix, translations, "from", "to_sector", "factors"
-        )
+        LOG.info("Balancing trip ends")
 
         balanced_adj_trip_ends = lgv_model.balance_trip_ends(
             adj_trip_ends.set_index("zone"),
@@ -213,9 +227,19 @@ class PriorAdjustmentMatrix:
             "column_targets",
             self.name,
         )
+        LOG.info("running gravity model")
+        sector_target_matrix = ctk.translation.pandas_matrix_zone_translation(
+            post_me_matrix,
+            create_network_to_sector_translation(translations),
+            "network",
+            "to_sector",
+            "factors",
+        )
+        sector_target_matrix.to_csv(
+            matrix_output_dir / f"{self.name}_sector_target_matrix.csv")
         cost_matrix = pd.read_csv(cost_matrix_path, index_col=0)
         cost_matrix.columns = [int(col) for col in cost_matrix.columns]
-        distribution = _4d_constraint_gravity_model(
+        distribution, sectoral_adj = _4d_constraint_gravity_model(
             balanced_adj_trip_ends,
             self.name,
             self.gm_inputs,
@@ -223,16 +247,24 @@ class PriorAdjustmentMatrix:
             translations,
             sector_target_matrix,
             calibrate=True,
-            csv_logging_path=output_path / f"{self.name}_gravity_model.csv",
+            csv_logging_path=matrix_output_dir / f"{self.name}_gravity_model.csv",
+        )
+        sectoral_adj.to_csv(
+            matrix_output_dir / f"{self.name}_sectoral_adjustment_factors.csv")
+
+        LOG.debug("Finished gravity model, processing results")
+
+        od_annual_matrix = process_annual_matrices(
+            distribution,
+            True,
+            balanced_adj_trip_ends,
+            cost_matrix,
+            matrix_output_dir,
+            translations,
+            self.name,
         )
 
-        # Balance trip ends
-
-        # 4d gravity model
-
-        # calculate trip distribution
-
-        pass
+        return od_annual_matrix
 
 
 def create_network_to_sector_translation(translation: pd.DataFrame) -> pd.DataFrame:
@@ -259,8 +291,8 @@ def calculate_trip_end_adjustment_factors(
     )
 
     row_prior_totals = sector_prior_matrix.sum(axis=1)
-    row_post_totals = sector_prior_matrix.sum(axis=1)
-    column_prior_totals = sector_post_matrix.sum(axis=0)
+    row_post_totals = sector_post_matrix.sum(axis=1)
+    column_prior_totals = sector_prior_matrix.sum(axis=0)
     column_post_totals = sector_post_matrix.sum(axis=0)
 
     # Calculate adjustment factors
@@ -297,10 +329,10 @@ class PriorAdjustmentInput(ctk.BaseConfig):
 
     def run(self):
         self.output_path.mkdir(parents=True, exist_ok=True)
-        (self.output_path / ANNUAL_MATRIX_DIR).mkdir(parents=True, exist_ok=True)
-
+        (self.output_path / INTERMEDIARY_MATRIX_DIR).mkdir(parents=True, exist_ok=True)
+        LOG.info("Creating annual matrices")
         self.create_annual_matrices(self.tp_annual_factors, self.output_path)
-
+        LOG.info("Creating zone lookup")
         demand_to_network = self.demand_to_network_path.read(
             factors_mandatory=False, generic_column_names=True
         )
@@ -317,20 +349,63 @@ class PriorAdjustmentInput(ctk.BaseConfig):
 
         prior_od_annual_matrix = self.prior_annual_totals(translations)
 
+        LOG.info("Creating time period factors")
+        tp_factors = sector_time_period_annual_factors(
+            self.post_me_matrix_path,
+            self.get_annual_post_od_matrix(),
+            create_network_to_sector_translation(translations),
+        )
+        with pd.ExcelWriter(self.output_path / "time_period_factors.xlsx") as writer:
+            for tp, factors in tp_factors.items():
+                factors.to_excel(writer, sheet_name=tp)
+
+        od_tp_matrices: dict[str, dict[str, pd.DataFrame]] = {}
+
+        tp_matrix_dir = self.output_path / "TP_matrices"
+        tp_matrix_dir.mkdir(parents=True, exist_ok=True)
+        
         for adjustment in self.adjustments:
-            adjustment.run(
+            LOG.info("Running adjustment %s", adjustment.name)
+            od_annual_matrix = adjustment.run(
                 self.post_me_purpose(prior_od_annual_matrix, translations, adjustment.name),
                 translations,
                 self.cost_matrix_path,
                 self.output_path,
             )
+            LOG.info("Finished adjustment %s, creating time period matrices", adjustment.name)
+            for tp, factors in tp_factors.items():
+                if od_tp_matrices.get(tp) is None:
+                    od_tp_matrices[tp] = {}
+                    (tp_matrix_dir / tp).mkdir(parents=True, exist_ok=True)
+                zonal_factors = ctk.translation.pandas_matrix_zone_translation(
+                    factors,
+                    translations,
+                    "to_sector",
+                    "from",
+                    "factors",
+                )
+                od_tp_matrices[tp][adjustment.name] = od_annual_matrix * zonal_factors
+                od_tp_matrices[tp][adjustment.name].to_csv(
+                    tp_matrix_dir / tp / f"{adjustment.name}_annual_od_matrix_{tp}.csv"
+                )
+        LOG.info(
+            "Finished creating time period matrices, creating combined matrix and convertinmg to noham"
+        )
+        for tp, od_matrices in od_tp_matrices.items():
+            tp_sum = reduce(lambda x, y: x.add(y, fill_value=0), od_matrices.values())
+            tp_sum.to_csv(tp_matrix_dir / f"{tp}_normits_od_matrix.csv")
+            ctk.translation.pandas_matrix_zone_translation(
+                tp_sum, translations, "from", "to_network", "factors"
+            ).to_csv(tp_matrix_dir / f"{tp}_noham_od_matrix.csv")
 
     @property
     def annual_post_od_matrix_path(self) -> str:
-        return f"{ANNUAL_MATRIX_DIR}/annual_post_od_matrix.csv"
+        return f"{INTERMEDIARY_MATRIX_DIR}/annual_post_od_matrix.csv"
 
     def get_annual_post_od_matrix(self) -> pd.DataFrame:
-        return pd.read_csv(self.output_path / self.annual_post_od_matrix_path, index_col=0)
+        matrix = pd.read_csv(self.output_path / self.annual_post_od_matrix_path, index_col=0)
+        matrix.columns = [int(col) for col in matrix.columns]
+        return matrix
 
     def create_annual_matrices(
         self, tp_to_annual_factors: dict[str, float], output_path: pathlib.Path
@@ -379,7 +454,8 @@ class PriorAdjustmentInput(ctk.BaseConfig):
         od_to_pa_factors = selected_adjustment.get_network_annual_pa_matrix(
             self.output_path, translation
         ) / selected_adjustment.get_network_annual_od_matrix(self.output_path, translation)
-
+        #TODO fillna with 0 or 1?
+        od_to_pa_factors = od_to_pa_factors.fillna(0)
         od_to_pa_factors.to_csv(self.output_path / f"{name}_od_to_pa_factors.csv")
 
         return post_od_purposes * od_to_pa_factors
@@ -388,6 +464,23 @@ class PriorAdjustmentInput(ctk.BaseConfig):
     def log_file(self):
         self.output_path.mkdir(parents=True, exist_ok=True)
         return self.output_path / "prior_adjustment.log"
+
+
+def sector_time_period_annual_factors(
+    tp_matrices: dict[str, pathlib.Path],
+    annual_matrix: pd.DataFrame,
+    translation: pd.DataFrame,
+) -> dict[str, pd.DataFrame]:
+    annual_gor_matrix = ctk.translation.pandas_matrix_zone_translation(
+        annual_matrix, translation, "network", "to_sector", "factors"
+    )
+    for tp, path in tp_matrices.items():
+        sector_tp_matrix = ctk.translation.pandas_matrix_zone_translation(
+            pd.read_csv(path, index_col=0), translation, "network", "to_sector", "factors"
+        )
+        tp_matrices[tp] = sector_tp_matrix / annual_gor_matrix
+
+    return tp_matrices
 
 
 def read_annual_matrix(
@@ -400,6 +493,7 @@ def read_annual_matrix(
             annual_matrix = pd.read_csv(path, index_col=0) / tp_to_annual_factors[tp]
         else:
             annual_matrix += pd.read_csv(path, index_col=0) / tp_to_annual_factors[tp]
+    
     return annual_matrix
 
 
@@ -412,7 +506,7 @@ def _4d_constraint_gravity_model(
     sector_target_matrix: pd.DataFrame,
     calibrate: bool,
     csv_logging_path: pathlib.Path,
-) -> lgv_model.VanGravityModelResults:
+) -> tuple[lgv_model.VanGravityModelResults:, pd.DataFrame]:
     """Internal function used in `run_gravity_model` for running the GM with calibration."""
 
     tld_path = gm_data.trip_length_distribution_path
@@ -485,6 +579,14 @@ def _4d_constraint_gravity_model(
         cost_function,
     )
 
+    gravity_model_results = calib_gm.calibrate(
+        cost_distributions,
+        csv_logging_path,
+        gravity_model.GMCalibParams(furness_jac=gm_data.furness_jacobian),
+        return_distributions=False,
+        verbose=2,
+    )
+
     sectoral_inputs = furness.SectoralConstraintInputs(
         constraint_area_trans,
         from_col="from",
@@ -492,23 +594,100 @@ def _4d_constraint_gravity_model(
         factor_col="factors",
         target_mat=sector_target_matrix,
         zonal_zones=zones,
+        # furness_inputs=furness.FurnessInputs(
+        #    seed_vals= calib_gm.achieved_distribution,
+        #    row_targets=trip_ends["row_targets"].to_numpy(),
+        #    column_targets=trip_ends["column_targets"].to_numpy(),
+    )
+    
+
+    matrix, sectoral_factors = furness.sectoral_constraint(
+        pd.DataFrame(calib_gm.achieved_distribution, index=zones, columns=zones),
+        sectoral_inputs,
     )
 
-    gravity_model_results = calib_gm.calibrate(
-        cost_distributions,
-        csv_logging_path,
-        gravity_model.GMCalibParams(furness_jac=gm_data.furness_jacobian),
-        return_distributions=False,
-        four_d_inputs=sectoral_inputs,
-        verbose=2,
-    )
+    results = lgv_model.VanGravityModelResults(matrix, zones, gravity_model_results)
 
-    results = lgv_model.VanGravityModelResults(
-        calib_gm.achieved_distribution, zones, gravity_model_results
-    )
+    return results, sectoral_factors
 
-    LOG.info("\tFinished, now writing outputs")
-    return results
+
+def process_annual_matrices(
+    calib_results: lgv_model.VanGravityModelResults,
+    is_pa_matrix: bool,
+    trip_ends: pd.DataFrame,
+    cost_matrix: pd.DataFrame,
+    output_folder: pathlib.Path,
+    sector_translation: pd.DataFrame,
+    name: str,
+) -> pd.DataFrame:
+    if is_pa_matrix:
+        # Save PA matrix to CSV and convert to OD dataframe
+
+        LOG.info("\tConverting PA to OD")
+
+        pa_matrix = pd.DataFrame(
+            calib_results.distribution, index=trip_ends.index, columns=trip_ends.index
+        )
+        pa_matrix.to_csv(output_folder / (name + "-trip_matrix-PA.csv"))
+
+        matrix = lgv_model.annual_pa_to_od(
+            calib_results.distribution.to_numpy(),
+            trip_ends.loc[calib_results.zones, "column_targets"].values,
+            trip_ends.loc[calib_results.zones, "row_targets"].values,
+        )
+
+        matrix = pd.DataFrame(
+            matrix,
+            index=calib_results.zones,
+            columns=calib_results.zones,
+        )
+    else:
+        matrix = pd.DataFrame(
+            calib_results.distribution,
+            index=calib_results.zones,
+            columns=calib_results.zones,
+        )
+
+    # Save the annual matrix, TLD graph and Excel summary file
+    matrix.to_csv(path_or_buf=output_folder / (name + "-trip_matrix-OD.csv"))
+
+    with pd.ExcelWriter(output_folder / (name + "-GM_log.xlsx")) as writer:
+        # TODO(kf) write out metadata
+
+        summary = ctk.pandas_utils.MatrixReport(
+            matrix,
+            translation_factors=sector_translation,
+            translation_from_col="from",
+            translation_to_col="to_sector",
+            translation_factors_col="factors",
+        )
+        LOG.info("writing %s summary to excel", name)
+        summary.write_to_excel(writer, output_sector_matrix=True)
+
+        for cat, gm_cat_results in calib_results.info.items():
+
+            gm_cat_results.plot_distributions().savefig(
+                output_folder / (name + f"-distribution_{cat}.pdf")
+            )
+            gm_cat_results.cost_distribution.df.to_excel(
+                writer, sheet_name=f"Achieved Distribution {cat}", index=False
+            )
+
+            gm_cat_results.target_cost_distribution.df.to_excel(
+                writer, sheet_name=f"Target Distribution {cat}", index=False
+            )
+
+        calib_results.summary.to_excel(writer, sheet_name="Gravity Model Info")
+
+        if is_pa_matrix:
+            vehicle_kms = lgv_model.calculate_vehicle_kms(pa_matrix, cost_matrix)
+            vehicle_kms.to_excel(writer, sheet_name="Vehicle Kilometres (PA)")
+
+        vehicle_kms = lgv_model.calculate_vehicle_kms(matrix, cost_matrix)
+        vehicle_kms.to_excel(writer, sheet_name="Vehicle Kilometres")
+    LOG.info("\tFinished writing")
+    return matrix
+
 
 
 if __name__ == "__main__":
@@ -516,5 +695,13 @@ if __name__ == "__main__":
     config = PriorAdjustmentInput.load_yaml("prior_adjustment.yml")
     with ctk.LogHelper(
         "caf", ctk.ToolDetails(__package__, __version__), log_file=config.log_file
-    ):
+    ) as log:
+        # accessing protected attribute is bad, but we have to so we can set the logging level
+        tqdm_log.logging_redirect_tqdm(
+            [log.logger, log._warning_logger]  # pylint: disable="protected-access"
+        )
+
+        log.add_console_handler(log_level=logging.DEBUG)
         config.run()
+
+
